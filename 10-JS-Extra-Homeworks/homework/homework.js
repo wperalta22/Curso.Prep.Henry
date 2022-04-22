934c49ea5d75cea3e72267aeb288589ff98836c0
wperalta22 <wwperalta@gmail.com>
2022-04-22 00:42:18 -0300

    Homework JS Extra finalizado

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var array = [];
+  for(var clave in objeto){
+    array.push([clave, objeto[clave]])
+  }
+  return array;
 }
 
 
@@ -18,6 +23,19 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var letras = [];
+  string.split("").forEach((element)=>{
+    if(!letras.includes(element)) letras.push(element)
+  })
+  var repeticiones = {};
+  letras.forEach((element)=>{
+    let contador = 0;
+    string.split("").forEach((elements)=>{
+      if(element===elements) contador+=1;
+    })
+    repeticiones[element]= contador;
+  })
+  return repeticiones;
 }
 
 
@@ -26,6 +44,17 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var mayusculas = [];
+  var minusculas = [];
+  s.split("").forEach((element) => {
+    if(element === element.toUpperCase()){
+      mayusculas.push(element);
+    }else if(element === element.toLowerCase()){
+      minusculas.push(element);
+    }
+  })
+  var Unir = [...mayusculas, ...minusculas];
+  return Unir.join("");
 }
 
 
@@ -35,7 +64,17 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  var palabrasOriginales = str.split(" ");   //["The", "Henry", "Challenge", "is", "close!"] 
+  var palabrasRevertidas = [];
+  palabrasOriginales.forEach((palabra)=>{
+    palabrasRevertidas.push(palabra.split("").reverse().join(""))
+  })
+  return palabrasRevertidas.join(" ");
 } 
+  //[T, h, e, H, e, n, r, y..] 
+  //[e, h, T, y, r, n, r,e,H...]
+  //["ehT", "yrneH", "egnellahC", "si!", "esolc"]
+  //ehT yrneH egnellahC si !esolc
 
 
 function capicua(numero){
@@ -43,6 +82,13 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var num = numero.toString();
+  var numInvertido = num.split("").reverse().join("");
+  if(num === numInvertido){
+    return "Es capicua";
+  } else{
+    return "No es capicua";
+  }
 }
 
 
@@ -50,6 +96,14 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var resultado = "";
+  for(var i = 0; i < cadena.length; i++){
+    if(cadena[i] !== "a" && cadena[i] !== "b" && cadena[i] !== "c"){
+      resultado += cadena[i];
+    }
+  }
+  return resultado;
+  
 }
 
 
@@ -57,6 +111,9 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  return arr.sort(function(a, b){ // si recibe 0, no hace nada
+    return a.length - b.length; // si recibe un nro. positivo, ordena b antes que a 
+  })                            // si recibe un nro. negativo, ordena a antes que b
 }
 
 
@@ -66,6 +123,15 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+  var newArray = [];
+  for(var i=0; i<arreglo1.length; i++){
+    for(var j=0; j<arreglo2.length; j++){
+      if(arreglo1[i]===arreglo2[j]){
+      newArray.push(arreglo2[j]);
+    }
+  }
+}
+  return newArray;
 }
 
 
